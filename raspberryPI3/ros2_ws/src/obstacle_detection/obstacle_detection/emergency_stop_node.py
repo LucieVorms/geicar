dab6abf796f77a5e829cf00a8e27e7d36709af5e
Toinouu <avyantoine.03@gmail.com>
2024-12-04 15:41:46 +0000

    Changed stop cond

--- raspberryPI3/ros2_ws/src/obstacle_detection/obstacle_detection/emergency_stop_node.py
+++ raspberryPI3/ros2_ws/src/obstacle_detection/obstacle_detection/emergency_stop_node.py
@@ -23,9 +23,9 @@ class EmergencyStopNode(Node):
         
         obstacle_detected = any([
 
-            msg.front_left < 60,
-            msg.front_center < 60,
-            msg.front_right < 60,
+            msg.front_left < 56,
+            msg.front_center < 56,
+            msg.front_right < 56,
                        ])
 
         
