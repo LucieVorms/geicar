625b2a23d94c0d6f656d41930a7dab739a2c73e4
diskouna <gnanguessim@insa-toulouse.fr>
2024-12-08 13:02:53 +0100

    First version of motors_control (discrete states : pwm =0/50/100)

--- raspberryPI3/ros2_ws/src/motors_control/src/motors_control.cpp
+++ raspberryPI3/ros2_ws/src/motors_control/src/motors_control.cpp
@@ -1,19 +1,99 @@
-#include <chrono>
 #include <functional>
+#include <chrono>
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 
+/* INTERFACE MESSAGES */
+#include "interfaces/msg/motors_order.hpp"
+#include "interfaces/msg/motors_feedback.hpp"
+#include "interfaces/msg/car_motion_order.hpp"
+
+/* CONFIGURATION */
+#define PERIOD_UPDATE_CMD 1ms 
+#define TOPIC_QUEUE_SIZE  10
+#define DEBUG_SPEED_0     1
+
 using namespace std;
+using placeholders::_1;
 
 class motors_control : public rclcpp::Node {
 public:
     motors_control()
     : Node("motors_control_node")
     {
-        RCLCPP_INFO(this->get_logger(), "motors_control_node READY");
+        leftRearPwmCmd_ = rightRearPwmCmd_ = steeringPwmCmd_ = 0;
+	subscription_car_motion_order_ = this->create_subscription<interfaces::msg::CarMotionOrder>(
+        "car_motion_order", 10, std::bind(&motors_control::carMotionOrderCallback, this, _1));
+
+        subscription_motors_feedback_ = this->create_subscription<interfaces::msg::MotorsFeedback>(
+        "motors_feedback", 10, std::bind(&motors_control::motorsFeedbackCallback, this, _1));
+
+        publisher_motors_order_ = this->create_publisher<interfaces::msg::MotorsOrder>("motors_order", 10);
+
+        timer_ = this->create_wall_timer(PERIOD_UPDATE_CMD, std::bind(&motors_control::controlLoop, this));
+        
+	RCLCPP_INFO(this->get_logger(), "motors_control_node READY");
     }
 
+private:
+
+    void motorsFeedbackCallback(const interfaces::msg::MotorsFeedback & motorsFeedback){
+        (void)motorsFeedback;
+	//TBD
+    }
+
+    /*
+       Update local motors' pwm order
+     */
+    void carMotionOrderCallback(const interfaces::msg::CarMotionOrder & carMotionOrder){
+     	int8_t car_speed            = carMotionOrder.car_speed;
+	int8_t front_wheel_rotation = carMotionOrder.front_wheel_rotation;
+
+	// Note: Simple passthrough controller
+	if (car_speed == 0)          leftRearPwmCmd_ = rightRearPwmCmd_ = 50;
+	else if (car_speed < 0)      leftRearPwmCmd_ = rightRearPwmCmd_ = 0;
+	else                         leftRearPwmCmd_ = rightRearPwmCmd_ = 100;
+
+	if (front_wheel_rotation == 0)          steeringPwmCmd_ = 50;
+	else if (front_wheel_rotation < 0)      steeringPwmCmd_ = 0;
+	else                                    steeringPwmCmd_ = 100;
+    }
+	
+    /*
+       Apply the local motor's pwm orders to the motors 
+       each 1 ms (1KHz control loop)
+     */
+    void controlLoop() {   
+        auto motorsOrder = interfaces::msg::MotorsOrder();
+
+#ifdef DEBUG_SPEED_0
+	motorsOrder.left_rear_pwm  = 50;
+        motorsOrder.right_rear_pwm = 50;
+        motorsOrder.steering_pwm   = 50;
+#else
+        motorsOrder.left_rear_pwm  = leftRearPwmCmd_;
+        motorsOrder.right_rear_pwm = rightRearPwmCmd_;
+        motorsOrder.steering_pwm   = steeringPwmCmd_;
+#endif  // DEBUG_SPEED_0        
+
+	publisher_motors_order_->publish(motorsOrder);
+    }
+
+    // Motors order
+    uint8_t leftRearPwmCmd_;
+    uint8_t rightRearPwmCmd_;
+    uint8_t steeringPwmCmd_;
+
+    //Publishers
+    rclcpp::Publisher<interfaces::msg::MotorsOrder>::SharedPtr publisher_motors_order_;
+
+    //Subscribers
+    rclcpp::Subscription<interfaces::msg::CarMotionOrder>::SharedPtr subscription_car_motion_order_;
+    rclcpp::Subscription<interfaces::msg::MotorsFeedback>::SharedPtr subscription_motors_feedback_;
+
+    //Timer
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 
