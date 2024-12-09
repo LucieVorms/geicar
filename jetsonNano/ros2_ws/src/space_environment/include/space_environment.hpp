529f59acdc0e4e4e9bfd1cf37fea0f433d1861f4
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-12-09 11:07:40 +0100

    SCRUM-82 v5 l'espoir
    Signed-off-by: Jean-Yves <7360784+docjyJ@users.noreply.github.com>

--- jetsonNano/ros2_ws/src/space_environment/include/space_environment.hpp
+++ jetsonNano/ros2_ws/src/space_environment/include/space_environment.hpp
@@ -1,16 +1,16 @@
 #ifndef SPACE_ENVIRONMENT_H
 #define SPACE_ENVIRONMENT_H
 
-//#include <space_environment_debug.hpp>
-#include <rclcpp/node.hpp>
-#include <sensor_msgs/msg/detail/laser_scan__struct.hpp>
-#include <std_msgs/msg/u_int8.hpp>
-#include <rclcpp/executors.hpp>
-
 
 #define SPACE_RADIUS 0.5
 #define SPACE_DEPTH 2.0
 #define ANGLE_OFFSET 0.0
+
+
+#include <rclcpp/executors.hpp>
+#include <rclcpp/node.hpp>
+#include <sensor_msgs/msg/detail/laser_scan__struct.hpp>
+#include "interfaces/msg/space_data.hpp"
 
 
 class SpaceEnvironment : public rclcpp::Node {
@@ -18,9 +18,8 @@ public:
     SpaceEnvironment();
 
 private:
-    // SpaceEnvironmentDebug *logger;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_subscription;
-    rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr enough_space_publisher;
+    rclcpp::Publisher<interfaces::msg::SpaceData>::SharedPtr enough_space_publisher;
 
     void scan_space(const std::shared_ptr<const sensor_msgs::msg::LaserScan> &msg) const;
 };
--- jetsonNano/ros2_ws/src/space_environment/src/space_environment.cpp
+++ jetsonNano/ros2_ws/src/space_environment/src/space_environment.cpp
@@ -6,35 +6,23 @@ SpaceEnvironment::SpaceEnvironment() : Node("space_environment") {
         scan_space(msg);
     };
     laser_scan_subscription = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, callback);
-    enough_space_publisher = this->create_publisher<std_msgs::msg::UInt8>("/have_enough_space", 10);
-    //
-    // RCLCPP_INFO(this->get_logger(), "SpaceEnvironment is Ready");
-    //
-    // logger = new SpaceEnvironmentDebug(this->get_logger());
+    enough_space_publisher = this->create_publisher<interfaces::msg::SpaceData>("/space_from_lidar", 10);
 }
 
 void SpaceEnvironment::scan_space(const std::shared_ptr<const sensor_msgs::msg::LaserScan> &msg) const {
     double rad = msg->angle_min + ANGLE_OFFSET;
     bool enough_space = true;
-//    logger->log_new_scan();
     for (const auto &range: msg->ranges) {
-        // logger->log_init(rad, range);
         if (range >= SPACE_DEPTH) {
-            // logger->log_space(true, SPACE_DEPTH);
         } else if (const auto space_radius = SPACE_RADIUS / std::cos(rad); range < space_radius) {
-            // logger->log_space(false, SPACE_DEPTH);
-            // logger->log_space(false, space_radius);
             enough_space = false;
             break;
-        } else {
-            // logger->log_space(false, SPACE_DEPTH);
-            // logger->log_space(true, space_radius);
         }
         rad += msg->angle_increment;
     }
 
-    std_msgs::msg::UInt8 msg_out;
-    msg_out.data = enough_space ? 1 : 0;
+    interfaces::msg::SpaceData msg_out;
+    msg_out.have_enough_space = enough_space ? 1 : 0;
     enough_space_publisher->publish(msg_out);
 }
 
