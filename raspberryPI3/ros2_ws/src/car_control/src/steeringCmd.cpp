e2469152d1a84ee173128628888859c900804bdc
Toinouu <avyantoine.03@gmail.com>
2024-12-10 18:09:40 +0000

    Modify steering function test this one

--- raspberryPI3/ros2_ws/src/car_control/src/steeringCmd.cpp
+++ raspberryPI3/ros2_ws/src/car_control/src/steeringCmd.cpp
@@ -1,40 +1,35 @@
 #include "../include/car_control/steeringCmd.h"
-/* Angles are specified in radians */
-#define STEERING_ANGLE_MAX     (+1.0f) // TBV
-#define STEERING_ANGLE_MIN     (-1.0f)
-#define STEERING_ANGLE_NUL     ( 0.0f) 
 
-#define STEERING_ANGLE_MAX_DEGREES (30.0f)
+#define MAX_ANGLE_ERROR 1.0 // Maximum error angle in normalized range [-1, 1]
+#define TOLERANCE_ANGLE 0.08 // Tolerance angle in normalized range [-1, 1]
+#define STOP 50
+#define DEADBAND 0.13 // Deadband around zero to handle sensor imprecision
 
-#define STEERING_DELTA_ANGLE_MAX ((STEERING_ANGLE_MAX) - (STEERING_ANGLE_NUL))
-#define STEERING_DELTA_ANGLE_MIN ((STEERING_ANGLE_MIN) - (STEERING_ANGLE_NUL))
-
-#define PWM_NUL_STEERING_ANGLE (50)
-#define MAX_DELTA_PWM          (50)
-
-//return the Pwm command to reach the angle passed in argument
-int steeringCmd(float requestedSteerAngle, float currentSteerAngle, uint8_t &steeringPwmCmd) {
-
-    float requestedSteerNormalized = requestedSteerAngle / STEERING_ANGLE_MAX_DEGREES;
-
-	
-	if (requestedSteerNormalized < STEERING_ANGLE_MIN) {
-		requestedSteerNormalized = STEERING_ANGLE_MIN;
-	} else if (requestedSteerNormalized > STEERING_ANGLE_MAX) {
-		requestedSteerNormalized = STEERING_ANGLE_MAX;
-	}
-
-    // Calcul de l'erreur
-    float errorAngle = requestedSteerNormalized - currentSteerAngle;
-
-    // Calcul de la commande PWM
-    if (abs(errorAngle) < TOLERANCE_ANGLE) {
-        steeringPwmCmd = PWM_NUL_STEERING_ANGLE;
-    } else {
-        float pwmAdjustment = (errorAngle * MAX_DELTA_PWM) / STEERING_DELTA_ANGLE_MAX;
-        steeringPwmCmd = PWM_NUL_STEERING_ANGLE + static_cast<int>(pwmAdjustment);
-
+// Return the Pwm command to reach the angle passed in argument
+int steeringCmd(float requestedSteerAngle, float currentSteerAngle, uint8_t & steeringPwmCmd) {
+    // Convert requestedSteerAngle from degrees to the normalized range [-1, 1]
+    float normalizedRequestedAngle = requestedSteerAngle / 30.0;
+    
+    // Apply deadband to currentSteerAngle to handle small variations around zero
+    if (abs(currentSteerAngle) < DEADBAND) {
+        currentSteerAngle = 0.0;
     }
 
-    return steeringPwmCmd;
+    // Compute the error angle
+    float errorAngle = currentSteerAngle - normalizedRequestedAngle;
+
+    // Command's calculation
+    if (abs(errorAngle) < TOLERANCE_ANGLE) {
+        steeringPwmCmd = STOP;
+    } else {
+        // Compute a proportional PWM value based on the error angle
+        float proportionalPwm = (abs(errorAngle) / MAX_ANGLE_ERROR) * 50; // 50 is the middle value for left and right
+        if (errorAngle > 0) {
+            steeringPwmCmd = 50 - proportionalPwm; // Turn left
+        } else {
+            steeringPwmCmd = 50 + proportionalPwm; // Turn right
+        }
+    }
+
+    return errorAngle;
 }
