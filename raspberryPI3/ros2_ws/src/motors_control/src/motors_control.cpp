ae695b56a9daa5010fcd317223fc28fb6ef025c2
diskouna <gnanguessim@insa-toulouse.fr>
2024-12-13 18:44:00 +0100

    Implement front wheel turning logic

--- raspberryPI3/ros2_ws/src/motors_control/src/motors_control.cpp
+++ raspberryPI3/ros2_ws/src/motors_control/src/motors_control.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <functional>
 #include <chrono>
 #include <memory>
@@ -24,6 +25,8 @@ public:
     motors_control()
     : Node("motors_control_node")
     {
+	car_speed_ = 0;
+	front_wheel_rotation_ = 0;
         leftRearPwmCmd_ = rightRearPwmCmd_ = steeringPwmCmd_ = 50;
 	subscription_car_motion_order_ = this->create_subscription<interfaces::msg::CarMotionOrder>(
         "car_motion_order", 10, std::bind(&motors_control::carMotionOrderCallback, this, _1));
@@ -40,30 +43,22 @@ public:
 
 private:
 
-    void motorsFeedbackCallback(const interfaces::msg::MotorsFeedback & motorsFeedback){
-        (void)motorsFeedback;
-	//TBD
-    }
-
-    /*
-       Update local motors' pwm order
-     */
-
 // TBD: create a config file for this :
 #define CAR_MAX_SPEED            10.0f    // ?? 	m/s
 #define FRONT_WHEEL_MAX_ROTATION 30.0f    // °
 
-    void carMotionOrderCallback(const interfaces::msg::CarMotionOrder & carMotionOrder){
-     	float car_speed            = carMotionOrder.car_speed;
-	float front_wheel_rotation = carMotionOrder.front_wheel_rotation;
+    void motorsFeedbackCallback(const interfaces::msg::MotorsFeedback & motorsFeedback){
+	currentFrontWheelRotation_ = motorsFeedback.steering_angle * FRONT_WHEEL_MAX_ROTATION;
+    }
 
+    /*
+       Update car motion order
+     */
+    void carMotionOrderCallback(const interfaces::msg::CarMotionOrder & carMotionOrder){
+     	car_speed_            = carMotionOrder.car_speed;
+     	front_wheel_rotation_ = carMotionOrder.front_wheel_rotation;
 	// assert (abs(car_speed) <= CAR_MAX_SPEED);
-	// assert (abs(front_wheel_rotation) <= front_wheel_rotation);
-	
-	// Rear wheels
-	leftRearPwmCmd_ = rightRearPwmCmd_ = 50 + static_cast<int8_t>(50 * car_speed/CAR_MAX_SPEED);
-	// Front wheel
-	steeringPwmCmd_ = 50 + static_cast<int8_t>(50 * front_wheel_rotation /FRONT_WHEEL_MAX_ROTATION);
+	// assert (abs(front_wheel_rotation) <= FRONT_WHEEL_MAX_ROTATION);
     }
 	
     /*
@@ -71,6 +66,18 @@ private:
        each 1 ms (1KHz control loop)
      */
     void controlLoop() {   
+	float diff_front_wheel_rotation = (front_wheel_rotation_ - currentFrontWheelRotation_); 
+	// assert (abs(diff_front_wheel_rotation) <= 2*FRONT_WHEEL_MAX_ROTATION);
+
+	// Rear wheels
+	leftRearPwmCmd_ = rightRearPwmCmd_ = 50 + static_cast<int8_t>(50 * car_speed_/CAR_MAX_SPEED);
+	// Front wheel
+	steeringPwmCmd_ = 50 + std::clamp(static_cast<int>(50 * diff_front_wheel_rotation /FRONT_WHEEL_MAX_ROTATION), -50, 50);
+	// Note: Some angles will be updated in more than one step.
+        // Eg : diff_front_wheel_rotation = 50 °
+	// step 1 : currentFrontWheelRotation_ += 30° (Max), diff_front_wheel_rotation = 20°
+	// step 2 : currentFrontWheelRotation_ += 20°      , diff_front_wheel_rotation =  0°   
+
         auto motorsOrder = interfaces::msg::MotorsOrder();
 
 #ifdef DEBUG_SPEED_0
@@ -86,10 +93,17 @@ private:
 	publisher_motors_order_->publish(motorsOrder);
     }
 
+    // Car motion order
+    float car_speed_;
+    float front_wheel_rotation_; 
+
     // Motors pwm (duty cycle) order
     uint8_t leftRearPwmCmd_;          // 0...100
     uint8_t rightRearPwmCmd_;         // 0...100
     uint8_t steeringPwmCmd_;          // 0...100
+
+    // Motors feedback
+    float currentFrontWheelRotation_;     
 
     //Publishers
     rclcpp::Publisher<interfaces::msg::MotorsOrder>::SharedPtr publisher_motors_order_;
