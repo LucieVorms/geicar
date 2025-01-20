bdd348351fa1b66b7617b993c829243c78fd825d
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2025-01-20 01:19:33 +0100

    Add PMRSpace

--- jetsonNano/ros2_ws/src/space_environment/src/space_environment.cpp
+++ jetsonNano/ros2_ws/src/space_environment/src/space_environment.cpp
@@ -1,46 +1,48 @@
 #include "space_environment.hpp"
 
 #include <functional>
+#include <cmath>
 
 using namespace std;
 using placeholders::_1;
 
-SpaceEnvironment::SpaceEnvironment() : Node("space_environment_node") {
+SpaceEnvironment::SpaceEnvironment() : Node("space_environment_node"),
+    max_depth(2.0), width(1.0), static_angle_offset(0.0) {
     subscription_laser_scan_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "scan", 10, std::bind(&SpaceEnvironment::LaserScanCallback, this, _1));
 
     publisher_enough_width_space_ = this->create_publisher<std_msgs::msg::Bool>("enough_width_space", 10);
 
-    max_depth = 2.0;
-    width = 1.0;
-    static_angle_offset = 0.0;
-
     RCLCPP_INFO(this->get_logger(), "Space environment node has been started.");
 }
 
 bool SpaceEnvironment::is_enough_space(const Scan &scan, const float offset) const {
-    double rad = scan.angle_reference;
-    const double field_of_view_offset = scan.angle_reference + static_angle_offset + offset;
-
-    for (const auto &value: scan.values) {
-        if (rad > M_PI) rad -= 2 * M_PI;
-        else if (rad < -M_PI) rad += 2 * M_PI;
-
-        if (
-            value < max_depth && rad < 0 &&
-            std::abs(value * std::cos(rad)) * 2 < width
-        )
+    float rad = scan.angle_reference + offset;
+    for (const float &value: scan.values) {
+        if (value < 0) continue; // Skip invalid values
+        rad = std::fmod(rad, 2 * M_PIf);
+        if (value < max_depth && rad > M_PIf && std::abs(value * std::cos(rad)) * 2 < width)
             return false;
-
         rad += scan.angle_increment;
     }
     return true;
 }
 
+bool SpaceEnvironment::is_enough_space_in_range(const Scan &scan, const float max_abs_angle) const {
+    if (is_enough_space(scan, 0.0)) return true;
+    float offset = scan.angle_increment;
+    while (offset <= max_abs_angle) {
+        if (is_enough_space(scan, offset)) return true;
+        if (is_enough_space(scan, -offset)) return true;
+        offset += scan.angle_increment;
+    }
+    return false;
+}
+
 void SpaceEnvironment::LaserScanCallback(const sensor_msgs::msg::LaserScan &msg) const {
     const auto scan = Scan{msg.angle_min, msg.angle_increment, msg.ranges};
     std_msgs::msg::Bool msg_out;
-    msg_out.data = is_enough_space(scan, 0.0);
+    msg_out.data = is_enough_space_in_range(scan, M_PI_4f);
 
     if (msg_out.data) {
         RCLCPP_INFO(this->get_logger(), "[X] True");
