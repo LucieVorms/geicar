c731ee14801b62a7987741013616d8b135abbfcc
Toinouu <avyantoine.03@gmail.com>
2025-01-22 21:32:01 +0000

    Hybrid V1

--- raspberryPI3/ros2_ws/src/gps_following/gps_following/gps_control.py
+++ raspberryPI3/ros2_ws/src/gps_following/gps_following/gps_control.py
@@ -22,7 +22,7 @@ class GnssListener(Node):
         self.publisher = self.create_publisher(GnssStatus, '/gnss_status', 10)
         
         # Load the GPS points from the itinerary CSV file
-        self.itinerary = self.load_itinerary_from_csv('gnss_data.csv')
+        self.itinerary = self.load_itinerary_from_csv('gnss_data_test.csv')
 
         if not self.itinerary:
             self.get_logger().error("No valid GPS points loaded from the itinerary!")
@@ -31,7 +31,7 @@ class GnssListener(Node):
         self.current_target_index = 0  # Start at the first target in the itinerary
         self.previous_lat = None
         self.previous_lon = None
-
+        self.lookahead_distance = 75
         # Define fixed offsets for latitude and longitude corrections (adjust as needed)
         self.lat_offset = 0.000025  # Example offset in latitude
         self.lon_offset = 0.000025  # Example offset in longitude
@@ -68,23 +68,51 @@ class GnssListener(Node):
 
          # Get the coordinates of the current target and the final destination
         target_lat, target_lon = self.itinerary[self.current_target_index]
-        final_lat, final_lon = self.itinerary[-1]  # Last point in the itinerary
 
         if self.previous_lat is not None and self.previous_lon is not None:
            # Calculate the distance to the target using the Haversine formula
+
+            reduced_itinerary = self.itinerary[self.current_target_index:]
+            current_position = (current_lat, current_lon)
+
+            pursuit_point, updated_index = self.calculate_pursuit_point(reduced_itinerary, current_position, self.lookahead_distance)
+
+            new_global_index = self.current_target_index + updated_index
+            if new_global_index > self.current_target_index:
+                self.current_target_index = new_global_index
+
+            if self.current_target_index == len(self.itinerary) - 1:
+                final_target = self.itinerary[-1]
+            if self.haversine(current_position[0], current_position[1], final_target[0], final_target[1]) < 50:
+                self.get_logger().info("Destination reached.")
+                status_msg.stop_following = True
+                rclpy.shutdown()
+
             distance = self.haversine(current_lat, current_lon, target_lat, target_lon)
             
             # Calculate the direction (bearing) towards the current position
             current_direction = self.calculate_bearing(self.previous_lat, self.previous_lon, current_lat, current_lon)
             
             # Calculate the direction (bearing) towards the new point
-            target_direction = self.calculate_bearing(current_lat, current_lon, target_lat,target_lon)  # Example target point
+            target_direction = self.calculate_bearing(current_lat, current_lon, pursuit_point[0],pursuit_point[1])  # Example target point
             
             # Calculate the angle difference between current and target directions
             angle_difference = self.calculate_angle_difference(current_direction, target_direction)
             
+            
+            if(distance < self.lookahead_distance):
+                status_msg.status_message = f"Arrived at target {self.current_target_index}. Moving to next target."
+                status_msg.stop_following = False
+            else: 
+                status_msg.status_message = f"Navigating to target {self.current_target_index}."
+                status_msg.stop_following = False
 
-
+            if angle_difference > 10:
+                status_msg.direction_message = f"Turn Right by {angle_difference:.2f} degrees"
+            elif angle_difference < -10:
+                status_msg.direction_message = f"Turn Left by {abs(angle_difference):.2f} degrees"
+            else:
+                status_msg.direction_message = "Go Straight"
 
             # Create a new status message
             status_msg = GnssStatus()
@@ -93,38 +121,13 @@ class GnssListener(Node):
             status_msg.target_latitude = target_lat
             status_msg.target_longitude = target_lon
             status_msg.distance_to_target = distance
-            status_msg.previous_latitude = self.previous_lat
-            status_msg.previous_longitude = self.previous_lon
+            status_msg.pursuit_latitude = pursuit_point[0]
+            status_msg.pursuit_longitude = pursuit_point[1]
             status_msg.current_direction = float(current_direction)
             status_msg.target_direction = target_direction
             status_msg.turn_angle = angle_difference
 
-            # Set the direction message based on the angle difference
-            if angle_difference > 10:
-                status_msg.direction_message = f"Turn Right by {angle_difference:.2f} degrees"
-            elif angle_difference < -10:
-                status_msg.direction_message = f"Turn Left by {abs(angle_difference):.2f} degrees"
-            else:
-                status_msg.direction_message = "Go Straight"
 
-        
-
-            # Check if the vehicle is close enough to the target (within 10 cm)
-            if distance < 30:  # 0.0001 km = 10 cm
-                # Move to the next target in the itinerary
-                self.current_target_index += 1
-                if self.current_target_index >= len(self.itinerary):
-                    status_msg.status_message = "End of itinerary reached!"
-                    self.current_target_index = 0  # Restart from the beginning of the itinerary
-                    status_msg.stop_following = True
-                else:
-                    status_msg.status_message = f"Arrived at target {self.current_target_index}. Moving to next target."
-                    status_msg.stop_following = False
-            else:
-                status_msg.status_message = f"Navigating to target {self.current_target_index}."
-                status_msg.stop_following = False
-
-            # Publish the status message
             self.publisher.publish(status_msg)
             
         self.previous_lat = current_lat
@@ -161,6 +164,27 @@ class GnssListener(Node):
         if angle_difference > 180:
             angle_difference -= 360
         return angle_difference
+
+    def calculate_pursuit_point(self,path, current_position, lookahead_distance):
+        for i in range(len(path) - 1):
+            start = current_position
+            end = path[i]
+            segment_length = self.haversine(current_position[0], current_position[1], end[0], end[1])
+            if segment_length >= lookahead_distance:
+                ratio = lookahead_distance / segment_length
+                pursuit_point_lat = start[0] + ratio * (end[0] - start[0])
+                pursuit_point_lon = start[1] + ratio * (end[1] - start[1])
+                
+                return (pursuit_point_lat, pursuit_point_lon),i
+        start = current_position
+        end = path[-1]
+        segment_length = self.haversine(start[0], start[1], end[0], end[1])
+        if segment_length >= lookahead_distance:
+            ratio = lookahead_distance / segment_length
+            pursuit_point_lat = start[0] + ratio * (end[0] - start[0])
+            pursuit_point_lon = start[1] + ratio * (end[1] - start[1])
+            return (pursuit_point_lat, pursuit_point_lon), len(path) - 1    
+        return path[-1],len(path)-1
 
 def main(args=None):
     rclpy.init(args=args)
